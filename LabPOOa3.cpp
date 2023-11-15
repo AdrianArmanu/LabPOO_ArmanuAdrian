245b8aa242154b6cad20930e85065a71072b3cb8
AdrianArmanu <148328645+AdrianArmanu@users.noreply.github.com>
2023-11-15 11:31:40 +0200

    Add files via upload

---
+++ LabPOOa3.cpp
@@ -0,0 +1,97 @@
+﻿#include <iostream>
+
+class NumarIntreg {
+private:
+    int valoare;
+
+public:
+    NumarIntreg(int val) : valoare(val) {}
+
+    // Operatorul de incrementare prefixat (++numar)
+    NumarIntreg& operator++() {
+        ++valoare;
+        return *this;
+    }
+
+    // Operatorul de incrementare sufixat (numar++)
+    NumarIntreg operator++(int) {
+        NumarIntreg temp(valoare);
+        ++valoare;
+        return temp;
+    }
+
+    // Operatorul de adunare cu un alt obiect de tip NumarIntreg
+    NumarIntreg operator+(const NumarIntreg& other) {
+        return NumarIntreg(valoare + other.valoare);
+    }
+
+    // Operatorul de adunare cu un număr întreg
+    NumarIntreg operator+(int num) {
+        return NumarIntreg(valoare + num);
+    }
+
+    // Funcția prietenă pentru operatorul de decrementare prefixat (--numar)
+    friend NumarIntreg operator--(NumarIntreg& num) {
+        --num.valoare;
+        return num;
+    }
+
+    // Funcția prietenă pentru operatorul de decrementare sufixat (numar--)
+    friend NumarIntreg operator--(NumarIntreg& num, int) {
+        NumarIntreg temp(num.valoare);
+        --num.valoare;
+        return temp;
+    }
+
+    // Funcția prietenă pentru operatorul de scădere cu un număr întreg
+    friend NumarIntreg operator-(const NumarIntreg& num, int val) {
+        return NumarIntreg(num.valoare - val);
+    }
+
+    // Funcția pentru afișarea valorii numerului
+    void afisare() {
+        std::cout << valoare << std::endl;
+    }
+};
+
+int main() {
+    NumarIntreg numar1(5);
+    NumarIntreg numar2(3);
+
+    // Operatorul de incrementare prefixat
+    std::cout << "Operatorul de incrementare prefixat" << std::endl;
+    ++numar1;
+    numar1.afisare();
+
+    // Operatorul de incrementare sufixat
+    std::cout << "Operatorul de incrementare sufixat" << std::endl;
+    numar2++;
+    numar2.afisare(); 
+
+    // Operatorul de adunare cu alt obiect NumarIntreg
+    std::cout << "Operatorul de adunare cu alt obiect NumarIntreg" << std::endl;
+    NumarIntreg rezultat = numar1 + numar2;
+    rezultat.afisare(); 
+
+    // Operatorul de adunare cu un număr întreg
+    std::cout << "Operatorul de adunare cu un numar intreg" << std::endl;
+    NumarIntreg rezultat2 = numar1 + 2;
+    rezultat2.afisare(); 
+
+    // Operatorul de decrementare prefixat
+    std::cout << "Operatorul de decrementare prefixat" << std::endl;
+    --numar1;
+    numar1.afisare();
+
+    // Operatorul de decrementare sufixat
+    std::cout << "Operatorul de decrementare sufixat" << std::endl;
+    numar2--;
+    numar2.afisare(); 
+
+    // Operatorul de scădere cu un număr întreg
+    std::cout << "Operatorul de scadere cu un numar intreg" << std::endl;
+    NumarIntreg rezultat3 = numar1 - 2;
+    rezultat3.afisare(); 
+
+    return 0;
+}
---
+++ LabPOOb3.cpp
@@ -0,0 +1,111 @@
+﻿#include <iostream>
+#include <set>
+
+class Set {
+private:
+    std::set<int> elements;
+
+public:
+    // Constructor implicit
+    Set() {}
+
+    // Constructor cu inițializare
+    Set(std::initializer_list<int> initList) {
+        for (const int& element : initList) {
+            elements.insert(element);
+        }
+    }
+
+    // Operatorul de uniune (+)
+    friend Set operator+(const Set& set1, const Set& set2) {
+        Set result = set1;
+        for (const int& element : set2.elements) {
+            result.elements.insert(element);
+        }
+        return result;
+    }
+
+    // Operatorul de intersecție (*)
+    friend Set operator*(const Set& set1, const Set& set2) {
+        Set result;
+        for (const int& element : set1.elements) {
+            if (set2.elements.count(element) > 0) {
+                result.elements.insert(element);
+            }
+        }
+        return result;
+    }
+
+    // Operatorul de scădere (-)
+    friend Set operator-(const Set& set1, const Set& set2) {
+        Set result = set1;
+        for (const int& element : set2.elements) {
+            result.elements.erase(element);
+        }
+        return result;
+    }
+
+    // Operatorul de inserare in mulțime (+=)
+    void operator+=(int element) {
+        elements.insert(element);
+    }
+
+    // Operatorul de comparare la egalitate (==)
+    bool operator==(const Set& other) const {
+        return elements == other.elements;
+    }
+
+    // Funcția de verificare a apartenenței unui element la mulțime
+    bool contains(int element) const {
+        return elements.count(element) > 0;
+    }
+
+    // Operatorul de citire (>>)
+    friend std::istream& operator>>(std::istream& in, Set& set) {
+        int element;
+        in >> element;
+        set.elements.insert(element);
+        return in;
+    }
+
+    // Operatorul de afișare (<<)
+    friend std::ostream& operator<<(std::ostream& out, const Set& set) {
+        for (const int& element : set.elements) {
+            out << element << ' ';
+        }
+        return out;
+    }
+};
+
+int main() {
+    Set set1 = { 1, 2, 3 };
+    Set set2 = { 3, 4, 5 };
+
+    Set unionSet = set1 + set2;
+    Set intersectionSet = set1 * set2;
+    Set differenceSet = set1 - set2;
+
+    std::cout << "Uniune: " << unionSet << std::endl;
+    std::cout << "Intersectie: " << intersectionSet << std::endl;
+    std::cout << "Diferenta: " << differenceSet << std::endl;
+
+    set1 += 6;
+    std::cout << "adaugam 6 in set1: " << set1 << std::endl;
+
+    if (set1 == set2) {
+        std::cout << "set1 si set2 sunt egale" << std::endl;
+    }
+    else {
+        std::cout << "set1 si set2 nu sunt egale" << std::endl;
+    }
+
+    int elementToCheck = 5;
+    if (set1.contains(elementToCheck)) {
+        std::cout << elementToCheck << " se afla in set1" << std::endl;
+    }
+    else {
+        std::cout << elementToCheck << " nu se afla in set1" << std::endl;
+    }
+
+    return 0;
+}
