284610dfadd2ce9ed3ee13609d1175ea32deea0a
AdrianArmanu <148328645+AdrianArmanu@users.noreply.github.com>
2023-11-29 11:30:07 +0200

    Add files via upload

---
+++ LabPOO4a.cpp
@@ -0,0 +1,108 @@
+﻿#include <iostream>
+#include <string>
+
+class Joc {
+public:
+    Joc(const std::string& nume) : nume_(nume) {
+        std::cout << "Constructor Joc: " << nume_ << std::endl;
+    }
+
+    virtual ~Joc() {
+        std::cout << "Destructor Joc: " << nume_ << std::endl;
+    }
+
+    Joc(const Joc& other) {
+        nume_ = other.nume_;
+        std::cout << "Constructor de copiere Joc: " << nume_ << std::endl;
+    }
+
+    Joc& operator=(const Joc& other) {
+        if (this != &other) {
+            nume_ = other.nume_;
+            std::cout << "Operator de atribuire Joc: " << nume_ << std::endl;
+        }
+        return *this;
+    }
+
+    virtual void desfasurare() const {
+        std::cout << "Desfasurare Joc: " << nume_ << std::endl;
+    }
+
+protected:
+    std::string nume_;
+};
+
+class JocSportiv : public Joc {
+public:
+    JocSportiv(const std::string& nume, const std::string& tipSport)
+        : Joc(nume), tipSport_(tipSport) {
+        std::cout << "Constructor JocSportiv: " << nume_ << ", " << tipSport_ << std::endl;
+    }
+
+    virtual ~JocSportiv() {
+        std::cout << "Destructor JocSportiv: " << nume_ << ", " << tipSport_ << std::endl;
+    }
+
+    JocSportiv(const JocSportiv& other) : Joc(other) {
+        tipSport_ = other.tipSport_;
+        std::cout << "Constructor de copiere JocSportiv: " << nume_ << ", " << tipSport_ << std::endl;
+    }
+
+    JocSportiv& operator=(const JocSportiv& other) {
+        if (this != &other) {
+            Joc::operator=(other);
+            tipSport_ = other.tipSport_;
+            std::cout << "Operator de atribuire JocSportiv: " << nume_ << ", " << tipSport_ << std::endl;
+        }
+        return *this;
+    }
+
+    void desfasurare() const override {
+        std::cout << "Desfasurare JocSportiv: " << nume_ << ", " << tipSport_ << std::endl;
+    }
+
+protected:
+    std::string tipSport_;
+};
+
+class Volei : public JocSportiv {
+public:
+    Volei(const std::string& nume, const std::string& tipSport, int numarJucatori)
+        : JocSportiv(nume, tipSport), numarJucatori_(numarJucatori) {
+        std::cout << "Constructor Volei: " << nume_ << ", " << tipSport_ << ", " << numarJucatori_ << " jucatori" << std::endl;
+    }
+
+    ~Volei() override {
+        std::cout << "Destructor Volei: " << nume_ << ", " << tipSport_ << ", " << numarJucatori_ << " jucatori" << std::endl;
+    }
+
+    Volei(const Volei& other) : JocSportiv(other) {
+        numarJucatori_ = other.numarJucatori_;
+        std::cout << "Constructor de copiere Volei: " << nume_ << ", " << tipSport_ << ", " << numarJucatori_ << " jucatori" << std::endl;
+    }
+
+    Volei& operator=(const Volei& other) {
+        if (this != &other) {
+            JocSportiv::operator=(other);
+            numarJucatori_ = other.numarJucatori_;
+            std::cout << "Operator de atribuire Volei: " << nume_ << ", " << tipSport_ << ", " << numarJucatori_ << " jucatori" << std::endl;
+        }
+        return *this;
+    }
+
+    void desfasurare() const override {
+        std::cout << "Desfasurare Volei: " << nume_ << ", " << tipSport_ << ", " << numarJucatori_ << " jucatori" << std::endl;
+    }
+
+private:
+    int numarJucatori_;
+};
+
+int main() {
+    Volei volei("Turneu de Volei", "Volei", 6);
+
+    std::cout << "\nApelarea functiei desfasurare:\n";
+    volei.desfasurare();
+
+    return 0;
+}
---
+++ LabPOO4b.cpp
@@ -0,0 +1,93 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+
+class Roata {
+public:
+    Roata(double raza) : raza_(raza) {
+        std::cout << "Constructor Roata: raza = " << raza_ << std::endl;
+    }
+
+    ~Roata() {
+        std::cout << "Destructor Roata: raza = " << raza_ << std::endl;
+    }
+
+    double getRaza() const {
+        return raza_;
+    }
+
+    void setRaza(double raza) {
+        raza_ = raza;
+    }
+
+private:
+    double raza_;
+};
+
+class Automobil {
+public:
+    Automobil(const std::string& firmaProducatoare) : firmaProducatoare_(firmaProducatoare) {
+        std::cout << "Constructor Automobil: Firma = " << firmaProducatoare_ << std::endl;
+    }
+
+    virtual ~Automobil() {
+        std::cout << "Destructor Automobil: Firma = " << firmaProducatoare_ << std::endl;
+    }
+
+    const std::string& getFirmaProducatoare() const {
+        return firmaProducatoare_;
+    }
+
+    void setFirmaProducatoare(const std::string& firmaProducatoare) {
+        firmaProducatoare_ = firmaProducatoare;
+    }
+
+    void adaugaRoata(const Roata& roata) {
+        roti_.push_back(roata);
+    }
+
+private:
+    std::vector<Roata> roti_;
+    std::string firmaProducatoare_;
+};
+
+class Autocamion : public Automobil {
+public:
+    Autocamion(const std::string& firmaProducatoare, double tonaj)
+        : Automobil(firmaProducatoare), tonaj_(tonaj) {
+        std::cout << "Constructor Autocamion: Firma = " << getFirmaProducatoare() << ", Tonaj = " << tonaj_ << std::endl;
+    }
+
+    ~Autocamion() override {
+        std::cout << "Destructor Autocamion: Firma = " << getFirmaProducatoare() << ", Tonaj = " << tonaj_ << std::endl;
+    }
+
+    double getTonaj() const {
+        return tonaj_;
+    }
+
+    void setTonaj(double tonaj) {
+        tonaj_ = tonaj;
+    }
+
+private:
+    double tonaj_;
+};
+
+int main() {
+    Roata roata1(15.5);
+    Roata roata2(15.5);
+    Roata roata3(16.0);
+    Roata roata4(16.0);
+
+    Automobil masina("Toyota");
+    masina.adaugaRoata(roata1);
+    masina.adaugaRoata(roata2);
+    masina.adaugaRoata(roata3);
+    masina.adaugaRoata(roata4);
+
+    Autocamion camion("Volvo", 5000.0);
+
+    return 0;
+}
+
